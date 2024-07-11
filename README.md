49d0a1d20ee7572b9c2fca9659f0bf4666223e6c
Luferath <107090091+Luferath@users.noreply.github.com>
2024-07-11 08:49:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-zpykz8
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Luferath/stackblitz-starters-zpykz8)
---
+++ script.js
@@ -0,0 +1,4 @@
+// Este script está aqui caso você queira adicionar funcionalidades extras no futuro
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Página carregada e script funcionando!');
+});
